ccdc37fb5d6d446b9f9c3bdb7c7b9b8bcc6acd2c
Oliver Adams <oliver.adams@gmail.com>
2020-11-13 12:57:25 +1100

    Added functioning pylint, mypy, and pytest calls by default.

--- {{ cookiecutter.repo_name }}/src/data/preprocess_data.py
+++ {{ cookiecutter.repo_name }}/src/data/preprocess_data.py
@@ -1,24 +1,25 @@
-# -*- coding: utf-8 -*-
-import click
+""" Preprocess data to prepare it for model training. """
+
 import logging
 from pathlib import Path
+from typing import Union
+
 from dotenv import find_dotenv, load_dotenv
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+def main(input_filepath: Union[str, Path], output_filepath: Union[str, Path]) -> None:
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    print(input_filepath)
+    print(output_filepath)
 
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    LOG_FMT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=LOG_FMT)
 
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
@@ -27,4 +28,4 @@ if __name__ == '__main__':
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
 
-    main()
+    main('data/raw', 'data/processed')
